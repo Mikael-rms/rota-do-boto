d7e52724fbc2cd60b05366aaad47a92add70fef5
Mikael <mikaelc623@gmail.com>
2026-04-17 22:11:00 -0300

    maintenance adicionada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,15 @@ import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import Layout from './view/components/Layout/layout.jsx'
+import AuthLayout from './view/components/AuthLayout/authLayout.jsx'
 
 import LoginPage from './view/loginPage/loginPage'
 import Home from './view/Home/home.jsx'
 import CadastroForm from './view/cadastro/cadastro.jsx'
 import Perfil from './view/perfilUsuario/perfil.jsx'
-import AuthLayout from './view/components/AuthLayout/authLayout.jsx'
+import Manutencao from './view/manutencao/manutencao.jsx'
+
+
 
 const router = createBrowserRouter ([
   {
@@ -35,6 +38,10 @@ const router = createBrowserRouter ([
         element: <Home />,
       },
     ],
+  },
+  {
+    path: "/manutencao",
+    element: <Manutencao />,
   },
 ]);
 
---
+++ frontend/src/view/manutencao/manutencao.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+function Manutencao() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-gray-800 mb-4">
+                Página em Manutenção
+            </h1>
+            <p className="text-lg text-gray-600">
+                Estamos trabalhando para melhorar sua experiência. Volte em breve!
+            </p>
+
+            <Link to="/" className="mt-6 bg-[#61EE9D] text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors">
+            Voltar 
+            </Link>
+        </div>
+    )
+} export default Manutencao;
