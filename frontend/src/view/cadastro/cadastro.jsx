64b9defad186f1c03d9f1645dfb448d5d46584a7
Mikael <mikaelc623@gmail.com>
2026-05-02 17:15:50 -0300

    feat: novo layout da pagina de cadastro

--- frontend/src/view/cadastro/cadastro.jsx
+++ frontend/src/view/cadastro/cadastro.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { auth } from "../../firebaseConfig";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-
 import { doc, setDoc } from "firebase/firestore";
 import { db } from "../../firebaseConfig";
-
 import { useNavigate } from "react-router-dom";
+
+import { User, FileText, CreditCard, Calendar, Phone, MapPin, LogOut, Ticket } from 'lucide-react';
+import CampoEntrada from "./components/campoEntrada";
 
 function CadastroForm() {
   const [email, setEmail] = useState("");
@@ -64,107 +65,82 @@ const handleCadastro = async () => {
   }
 };
   return (
-    <section className="w-full min-h-screen bg-gray-100 py-6 md:py-10">
-      
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 font-sans">
+    <div className="min-h-screen bg-gray-50 p-4 md:p-12 flex items-center justify-center w-full">
+      <div className="w-full max-w-6xl flex flex-col md:flex-row gap-8 ml-0">
+       <main className="flex-1 bg-white border border-gray-100 rounded-lg shadow-sm overflow-hidden text-left">
 
-        <div className="border border-gray-300 rounded-[30px] md:rounded-[40px] p-6 md:p-10 shadow-sm bg-white w-full">
+        <div className="p-8 pt-4">
+        <div className="flex flex-col gap-10">
+        
+        {/* Dados pessoais */}
+        <section>
+         <div className="px-8 pt-8 pb-4 text-left">
+            <h1 className="text-sky-500 text-2xl font-bold mb-4 text-left">Dados pessoais</h1>
+            <hr className="border-gray-200" />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-6">
+            <CampoEntrada rotulo="Nome" icone={User} tipo="text" campo = {(e) => setNome(e.target.value)} />
+            <CampoEntrada rotulo="CPF" icone={CreditCard} tipo="text" campo = {(e) => setCpf(e.target.value)} />
+            <CampoEntrada rotulo="Data de Nascimento" icone={Calendar} tipo="date" campo = {(e) => setNascimento(e.target.value)} />
+            <CampoEntrada rotulo="Telefone" icone={Phone} tipo="tel" campo = {(e) => setTelefone(e.target.value)} />
+          </div>
+        </section>
+
+        {/* Endereço */}
+        <section>
+          <div className="px-8 pt-8 pb-4 text-left">
+            <h1 className="text-sky-500 text-2xl font-bold mb-4 text-left">Endereço</h1>
+            <hr className="border-gray-200" />
+        </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-6">
+            <CampoEntrada rotulo="CEP" tipo="number" campo={(e) => setCep(e.target.value)} />
+           
+            <div className="flex flex-col gap-2">
+            <label className="text-gray-500 text-xs font-semibold uppercase">Município</label>
+             <select 
+              value={municipio}
+              onChange={(e) => setMunicipio(e.target.value)}
+              className="border border-gray-300 rounded-md px-3 py-2 text-sm text-gray-700 outline-none focus:border-sky-500 bg-white cursor-pointer">
+              <option value="">Selecione seu município</option>
           
-          <div className="mb-8">
-            <h2 className="text-2xl font-semibold mb-6">Dados pessoais</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <div>
-                <label className="block text-xs font-bold text-gray-500 uppercase mb-2 ml-1">Nome Completo</label>
-                <input type="text" onChange={(e) => setNome(e.target.value)}
-                className="w-full border border-gray-300 rounded-full py-3 px-5 outline-none focus:border-green-500" />
-              </div>
-              <div>
-                <label className="block text-xs font-bold text-gray-500 uppercase mb-2 ml-1">Telefone</label>
-                <input type="text" onChange={(e) => setTelefone(e.target.value)}
-                className="w-full border border-gray-300 rounded-full py-3 px-5 outline-none focus:border-green-500" />
-              </div>
-              <div>
-                <label className="block text-xs font-bold text-gray-500 uppercase mb-2 ml-1">CPF</label>
-                <input type="text" onChange={(e) => setCpf(e.target.value)}
-                className="w-full border border-gray-300 rounded-full py-3 px-5 outline-none focus:border-green-500" />
-              </div>
-              <div>
-                <label className="block text-xs font-bold text-gray-500 uppercase mb-2 ml-1">Data de Nascimento</label>
-                <input type="text" onChange={(e) => setNascimento(e.target.value)}
-                className="w-full border border-gray-300 rounded-full py-3 px-5 outline-none focus:border-green-500" />
-              </div>
+              {municipios.sort().map((c, index) => (
+              <option key={index} value={c}>{c} </option>
+              ))}
+             </select>
             </div>
+            
+            <CampoEntrada rotulo="Bairro" tipo="text" campo={(e) => setBairro(e.target.value)} />
+            <CampoEntrada rotulo="Logradouro" tipo="text" campo={(e) => setLogradouro(e.target.value)} />
+            <CampoEntrada rotulo="Número" tipo="number" campo={(e) => setNumero(e.target.value)} />
           </div>
+        </section>
 
-          <hr className="border-gray-300 mb-8" />
-
-          <div className="mb-8">
-            <h2 className="text-2xl font-semibold mb-6">Dados de acesso</h2>
-            <div className="mb-6">
-              <label className="block text-xs font-bold text-gray-500 uppercase mb-2 ml-1">Seu Email</label>
-              <input type="email" onChange={(e) => setEmail(e.target.value)} 
-              className="w-full border border-gray-300 rounded-full py-3 px-5 outline-none focus:border-green-500" />
-            </div>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <div>
-                <label className="block text-xs font-bold text-gray-500 uppercase mb-2 ml-1">Senha</label>
-                <input type="password" onChange={(e) => setSenha(e.target.value)} 
-                className="w-full border border-gray-300 rounded-full py-3 px-5 outline-none focus:border-green-500" />
-              </div>
-            </div>
+        {/* dados de acesso */}
+        <section>
+          <div className="px-8 pt-8 pb-4 text-left">
+            <h1 className="text-sky-500 text-2xl font-bold mb-4 text-left">Dados de acesso</h1>
+            <hr className="border-gray-200" />
+        </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-6">
+            <CampoEntrada rotulo="Email" tipo="email" campo={(e) => setEmail(e.target.value)} />
+            <CampoEntrada rotulo="Senha" tipo="password" campo={(e) => setSenha(e.target.value)} />
           </div>
+        </section>
 
-          <hr className="border-gray-300 mb-8" />
-
-          <div className="mb-10">
-            <h2 className="text-2xl font-semibold mb-6">Endereço</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <div>
-                <label className="block text-xs font-bold text-gray-500 uppercase mb-2 ml-1">CEP</label>
-                <input type="text" onChange={(e) => setCep(e.target.value)} 
-                className="w-full border border-gray-300 rounded-full py-3 px-5 outline-none focus:border-green-500" />
-              </div>
-              <div>
-                <label className="block text-xs font-bold text-gray-500 uppercase mb-2 ml-1">Bairro</label>
-                <input type="text" onChange={(e) => setBairro(e.target.value)} 
-                className="w-full border border-gray-300 rounded-full py-3 px-5 outline-none focus:border-green-500" />
-              </div>
-              <div>
-                <label className="block text-xs font-bold text-gray-500 uppercase mb-2 ml-1">Município</label>
-                <select value={municipio} onChange={(e) => setMunicipio(e.target.value)} className="w-full border border-gray-300 rounded-full py-3 px-5 outline-none focus:border-green-500">
-                  <option value="">Selecione um município</option>
-
-                  {municipios.map((c, index) => (
-                    <option key={index} value={c}>
-                      {c}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div>
-                <label className="block text-xs font-bold text-gray-500 uppercase mb-2 ml-1">Logradouro</label>
-                <input type="text" onChange={(e) => setLogradouro(e.target.value)} 
-                className="w-full border border-gray-300 rounded-full py-3 px-5 outline-none focus:border-green-500" />
-              </div>
-              <div>
-                <label className="block text-xs font-bold text-gray-500 uppercase mb-2 ml-1">Numero</label>
-                <input type="text" onChange={(e) => setNumero(e.target.value)} 
-                className="w-full border border-gray-300 rounded-full py-3 px-5 outline-none focus:border-green-500" />
-              </div>
-            </div>
-          </div>
-
-          <div className="flex justify-center w-full">
+        <div className="flex justify-center w-full">
             <button onClick={handleCadastro}
-            className="w-full sm:w-80 bg-sky-500 text-black font-semibold py-3 rounded-xl shadow-md hover:brightness-95 transition-all">
+            className="w-full sm:w-80 bg-sky-500 text-gray-700 font-semibold py-3 rounded-xl shadow-md hover:brightness-95 transition-all">
               Registrar
             </button>
           </div>
 
-        </div>
       </div>
-    </section>
+  </div>
+
+        </main>
+      </div>
+    </div>
+
   );
 }
 
